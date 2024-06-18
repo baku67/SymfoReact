b59a99ed55207b36e6bc6da2e5226c7d8ad166e8
baku67 <basile08@hotmail.fr>
2024-06-19 01:16:22 +0200

    npm run build sur route "base.html.twig" Symfony

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+* React frontend:
+npm run build, move dossier build into ../backend/public (Pas besoin de rebuild, modifs reflétées)
+npm start
+
+* Symfony backend:
+symfony serve
+
+
+http://localhost:3000/ (/)
+http://localhost:8000/ (test symfony ok)
--- backend/src/Controller/DefaultController.php
+++ backend/src/Controller/DefaultController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    // #[Route('/', name: 'app_home')]
+    /**
+     * @Route("/{reactRouting}", name="app", requirements={"reactRouting"="^(?!api).+"})
+     */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('base.html.twig');
     }
 }
